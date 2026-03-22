c1867cfb761a6dcecb253417305533e7cc922af4
Chisadin <tranthanhnam.ccc@gmail.com>
2026-03-22 23:04:16 +0700

    fix(hmac): change payload type to string and update HMAC generation logic

--- payment/src/module/hmac.ts
+++ payment/src/module/hmac.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 import { MBTransaction } from "../types/mbbank";
 
-export const generateHMAC = (payload: MBTransaction) => {
+export const generateHMAC = (payload: string) => {
     const secretKey = process.env.HMAC
     if (!secretKey) {
         throw new Error("HMAC secret key is not defined in environment variables.");
@@ -10,6 +10,5 @@ export const generateHMAC = (payload: MBTransaction) => {
         throw new Error("Payload to be hashed is required.");
     }
     
-    const data = JSON.stringify(payload);
-    return crypto.createHmac("sha256", secretKey).update(data).digest("hex");
+    return crypto.createHmac("sha256", secretKey).update(payload).digest("hex");
 }
